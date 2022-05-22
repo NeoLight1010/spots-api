1ff771c6f44693c2b93518560cfed30f01079fe5
Anthony Suárez <neolight1010@gmail.com>
2022-05-22 15:50:35 -0500

    Learning: Print posts in rocket().

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use db::setup::establish_connection;
+use db::{models::Post, schema::posts::dsl::*, setup::establish_connection};
+use diesel::prelude::*;
+use dotenv::dotenv;
 
 #[macro_use]
 extern crate rocket;
@@ -15,7 +17,22 @@ fn hello_world() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    let db_conn = establish_connection();
+    dotenv().ok();
+
+    let connection = establish_connection();
+
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts:", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------");
+        println!("{}", post.body);
+    }
 
     rocket::build().mount("/", routes![hello_world])
 }
