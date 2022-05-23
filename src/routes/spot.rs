807bc8686dfe4aa529dbc8807c52bdf2e5d5f2cf
Anthony Suárez <neolight1010@gmail.com>
2022-05-23 10:57:49 -0500

    Return all spots on spot.index.

--- src/routes/spot.rs
+++ src/routes/spot.rs
@@ -1,4 +1,17 @@
+use crate::db::{models::Spot, schema::spots::dsl::*, setup::DBPool};
+
+use diesel::prelude::*;
+use rocket::serde::json::Json;
+
 #[get("/")]
-pub async fn index() -> &'static str {
-    "Spots list."
+pub async fn index(db: DBPool) -> Json<Vec<Spot>> {
+    let all_spots: Vec<Spot> = db
+        .run(|conn| {
+            spots
+                .get_results(conn)
+                .expect("Error loading spots.")
+        })
+        .await;
+
+    Json(all_spots)
 }
