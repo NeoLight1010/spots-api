fac3878a2af077708cb35b70a9db195df4261780
Anthony Suárez <neolight1010@gmail.com>
2022-05-23 11:09:28 -0500

    Derive Deserializable for Spot.

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,9 +1,9 @@
+use super::schema::spots;
 use diesel::Queryable;
 use diesel_geometry::data_types::PgPoint;
-use serde::Serialize;
-use super::schema::spots;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Serialize)]
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct Spot {
     pub id: i32,
     pub title: String,
@@ -12,7 +12,7 @@ pub struct Spot {
 }
 
 #[derive(Insertable)]
-#[table_name="spots"]
+#[table_name = "spots"]
 pub struct NewPost<'a> {
     pub title: &'a str,
     pub description: &'a str,
