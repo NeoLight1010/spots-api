6ffb35b342bd24f7ac43d571773eb2bf785f890f
Anthony Suárez <neolight1010@gmail.com>
2022-05-29 11:48:13 -0500

    Return Result on register_user.

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,9 +1,10 @@
 use crate::db::{
     connection::DBConn,
-    models::user::{NewUserNotEncrypted, User, NewUserEncrypted},
+    models::user::{NewUserEncrypted, NewUserNotEncrypted, User},
     schema::users::dsl::*,
 };
-use diesel::prelude::*;
+use diesel::result::Error;
+use diesel::{prelude::*, result::DatabaseErrorKind::UniqueViolation};
 use rocket::serde::json::Json;
 
 #[get("/")]
@@ -17,15 +18,24 @@ pub fn get_users(conn: DBConn) -> Json<Vec<User>> {
 pub fn register_user(
     conn: DBConn,
     new_user_not_encrypted_json: Json<NewUserNotEncrypted>,
-) -> Json<User> {
+) -> Result<Json<User>, String> {
     let new_user_not_encrypted = new_user_not_encrypted_json.into_inner();
 
     let new_user_encrypted = NewUserEncrypted::new(&new_user_not_encrypted);
 
-    let new_user = diesel::insert_into(users)
+    let new_user_result: Result<User, _> = diesel::insert_into(users)
         .values(&new_user_encrypted)
-        .get_result(&*conn)
-        .expect("Error inserting new user.");
+        .get_result(&*conn);
 
-    Json(new_user)
+    match new_user_result {
+        Ok(new_user) => Ok(Json(new_user)),
+        Err(error) => match error {
+            Error::DatabaseError(db_error, _) => match db_error {
+                UniqueViolation => Err("Username already exists.".into()),
+                _ => Err(error.to_string()),
+            },
+
+            _ => Err(error.to_string()),
+        },
+    }
 }
