5e40f3b2b0415c0183a15bde7614e0d8517f9a31
Anthony Suárez <neolight1010@gmail.com>
2022-05-29 10:56:34 -0500

    Create NewUserEncrypted::new.

--- src/db/models/user.rs
+++ src/db/models/user.rs
@@ -1,5 +1,6 @@
 use crate::db::schema::users;
-use diesel::{Insertable, Queryable};
+
+use diesel::Queryable;
 use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, Serialize)]
@@ -21,4 +22,52 @@ pub struct NewUserNotEncrypted {
 pub struct NewUserEncrypted {
     pub username: String,
     pub password: String,
+}
+
+impl NewUserEncrypted {
+    pub fn new(new_user_not_encrypted: &NewUserNotEncrypted) -> NewUserEncrypted {
+        let hashed_password = 
+            Self::hash_password(new_user_not_encrypted.password.as_str());
+
+        NewUserEncrypted {
+            username: new_user_not_encrypted.username.clone(),
+            password: hashed_password,
+        }
+    }
+
+    fn hash_password(password: &str) -> String {
+        let salt = b"random_salt";
+
+        let config = argon2::Config::default();
+
+        let hashed = argon2::hash_encoded(password.as_bytes(), salt, &config)
+            .expect("Error hashing password.");
+
+        return hashed;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{NewUserEncrypted, NewUserNotEncrypted};
+
+    #[test]
+    fn new_user_encrypted_should_encrypt_password() {
+        let non_encrypted_password = "my_password";
+
+        let new_user_not_encrypted = NewUserNotEncrypted {
+            username: "test".into(),
+            password: non_encrypted_password.into(),
+        };
+
+        let new_user_encrypted = NewUserEncrypted::new(&new_user_not_encrypted);
+
+        let matches = argon2::verify_encoded(
+            &new_user_encrypted.password,
+            non_encrypted_password.as_bytes(),
+        )
+        .unwrap();
+
+        assert!(matches);
+    }
 }
