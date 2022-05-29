ebe1c066a0a3073675ddbbc42f55cb52fad58172
Anthony Suárez <neolight1010@gmail.com>
2022-05-29 17:52:22 -0500

    Write tests for login module.

--- src/auth/login.rs
+++ src/auth/login.rs
@@ -20,5 +20,58 @@ pub fn validate_login(optional_user: Option<User>, raw_password: &str) -> Result
 }
 
 fn validate_password(raw_password: &str, hashed_password: &str) -> bool {
-    argon2::verify_encoded(hashed_password, raw_password.as_bytes()).unwrap()
+    argon2::verify_encoded(hashed_password, raw_password.as_bytes()).unwrap_or(false)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::db::models::user::User;
+
+    use super::{validate_login, LoginError};
+
+    #[test]
+    fn validate_login_username_does_not_exist() {
+        let result = validate_login(None, "");
+        let error = result.err().unwrap();
+
+        match error {
+            LoginError::UsernameDoesNotExist => (),
+            _ => panic!("Incorrect error.")
+        };
+    }
+
+    #[test]
+    fn validate_login_incorrect_password() {
+        let correct_password = "my-test-password";
+        let incorrect_password = "this-password-is-not-correct";
+
+        let user = User {
+            id: 1,
+            username: "test".into(),
+            password: correct_password.into(),
+        };
+
+        let result = validate_login(Some(user), incorrect_password);
+        let error = result.err().unwrap();
+
+        match error {
+            LoginError::WrongPassword => (),
+            _ => panic!("Incorrect error.")
+        };
+    }
+
+    #[test]
+    fn validate_login_successful() {
+        let correct_password = "my-test-password";
+
+        let user = User {
+            id: 1,
+            username: "test".into(),
+            password: correct_password.into(),
+        };
+
+        let result = validate_login(Some(user), correct_password);
+
+        result.ok().unwrap();
+    }
 }
