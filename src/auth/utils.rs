cc9c5fe1a6d8a966c8a0b5721d91fe40cdef2e2e
Anthony Suárez <neolight1010@gmail.com>
2022-05-29 18:33:33 -0500

    Add tests for auth::utils.

--- src/auth/utils.rs
+++ src/auth/utils.rs
@@ -12,3 +12,37 @@ pub fn encrypt_password(password: &str) -> String {
 pub fn validate_password(raw_password: &str, hashed_password: &str) -> bool {
     argon2::verify_encoded(hashed_password, raw_password.as_bytes()).unwrap_or(false)
 }
+
+#[cfg(test)]
+mod test {
+    use super::{encrypt_password, validate_password};
+
+    #[test]
+    fn encrypt_should_not_return_same_password() {
+        let raw_password = "my_raw_password";
+        let encrypted_password = encrypt_password(raw_password);
+
+        assert_ne!(raw_password, encrypted_password);
+    }
+
+    #[test]
+    fn validate_correct_password() {
+        let raw_password = "my_raw_password";
+        let encrypted_password = encrypt_password(raw_password);
+
+        let matches = validate_password(raw_password, &encrypted_password);
+
+        assert!(matches);
+    }
+
+    #[test]
+    fn validate_incorrect_password() {
+        let encrypted_correct_password = encrypt_password("my_correct_password");
+
+        let raw_incorrect_password = "my_incorrect_password";
+
+        let matches = validate_password(raw_incorrect_password, &encrypted_correct_password);
+
+        assert!(!matches);
+    }
+}
