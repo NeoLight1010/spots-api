9172903e9268df6dae57c6a6dcc622d3efb8e559
Anthony Suárez <neolight1010@gmail.com>
2022-05-31 21:32:02 -0500

    Add DBError type to DBConn FromRequest.

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -10,16 +10,19 @@ use super::setup::DBPool;
 
 pub struct DBConn(PooledConnection<ConnectionManager<PgConnection>>);
 
+#[derive(Debug)]
+pub struct DBError {}
+
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for DBConn {
-    type Error = ();
+    type Error = DBError;
 
     async fn from_request(request: &'r Request<'_>) -> Outcome<DBConn, Self::Error> {
          let pool = request.rocket().state::<DBPool>().unwrap();
 
          match pool.get() {
              Ok(conn) => Outcome::Success(DBConn(conn)),
-             Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+             Err(_) => Outcome::Failure((Status::ServiceUnavailable, DBError {},))
          }
     }
 }
